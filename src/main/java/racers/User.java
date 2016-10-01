82047a371f57a20123a6da335c60a043c372708a
andredly <emubem1@gmail.com>
2016-10-01 22:17:42 +0300

    Test update

---
+++ src/main/java/racers/User.java
@@ -0,0 +1,146 @@
+package racers;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Date date;
+    private enum sex{MR,MRS,MISS,MS;};
+    private String country;
+    private String city;
+    private List<String> address;
+    private String postCode;
+    private String login;
+    private String password;
+    private String currency;
+    private String securityQuestion;
+    private String answer;
+    private String email;
+    private Balans userBalanse;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Balans getUserBalanse() {
+        return userBalanse;
+    }
+
+    public void setUserBalanse(Balans userBalanse) {
+        this.userBalanse = userBalanse;
+    }
+
+
+
+}
