e0d453f23009332f17367dc2e1264fb736369307
andredly <emubem1@gmail.com>
2016-11-28 16:39:21 +0300

    service

---
+++ webapp/src/main/java/com/charniauski/training/horsesrace/web/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.charniauski.training.horsesrace.web.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.charniauski.training.horsesrace.web.springmvc")
+public class HelloWorldConfiguration {
+	
+
+}
---
+++ webapp/src/main/java/com/charniauski/training/horsesrace/web/springmvc/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.charniauski.training.horsesrace.web.springmvc.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
