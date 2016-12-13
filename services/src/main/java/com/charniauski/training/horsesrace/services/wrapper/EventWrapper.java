b31c4eddcfc8e876d22e531f7371a53d5a1d1335
andredly <emubem1@gmail.com>
2016-12-13 17:02:20 +0300

    service

--- services/src/main/java/com/charniauski/training/horsesrace/services/wrapper/EventWrapper.java
+++ services/src/main/java/com/charniauski/training/horsesrace/services/wrapper/EventWrapper.java
@@ -1,9 +1,11 @@
 package com.charniauski.training.horsesrace.services.wrapper;
 
 import com.charniauski.training.horsesrace.datamodel.Event;
+import com.charniauski.training.horsesrace.datamodel.Horse;
 import com.charniauski.training.horsesrace.datamodel.RaceDetail;
 
 public class EventWrapper {
     private Event event;
     private RaceDetail raceDetail;
+    private Horse horse;
 }
